4c258fa7ca94e1651d03b8aa0808666843defd6d
WuChang0124 <c01240214@gmail.com>
2025-05-11 06:16:02 +0000

    bug fix

--- scripts/data_process/utils.py
+++ scripts/data_process/utils.py
@@ -18,14 +18,7 @@ After reasoning, if you find you lack some knowledge, you can call a search engi
 After each search, you need to summarize and refine the existing documents in <refine> and </refine>. \
 You can search as many times as your want. \
 If you find no further external knowledge needed, you can directly provide the answer inside <answer> and </answer>, without detailed illustrations. For example, <answer> Beijing </answer>. Question: {question}\n"""
-    elif template_type == 'refine_overhaul':
-        prefix = f"""You are a helpful assistant excel at answering questions with multi-turn search engine calling. \
-To answer questions, you must first reason through the available information using <think> and </think>. \
-If you identify missing knowledge, you may issue a search request using <search> query </search> at any time. The retrieval system will provide you with the three most relevant documents enclosed in <documents> and </documents>. \
-After each retrieval, you are required to perform knowledge refinement using <refine> and </refine>—extracting key information and distilling the content of all available documents. \
-You may repeat the cycle of <think>, <search>, and <refine> as needed. \
-Once you have sufficient information, provide a concise final answer using <answer> and </answer>. For example, <answer> Donald Trump </answer>. Question: {question}\n"""
-    elif template_type == 'refine_overhaul_2':
+    elif template_type == 'autorefine':
         prefix = f"""You are a helpful assistant excel at answering questions with multi-turn search engine calling. \
 To answer questions, you must first reason through the available information using <think> and </think>. \
 If you identify missing knowledge, you may issue a search request using <search> query </search> at any time. The retrieval system will provide you with the three most relevant documents enclosed in <documents> and </documents>. \
--- scripts/nq_hotpotqa/data_process_sort.sh
+++ scripts/nq_hotpotqa/data_process_sort.sh
@@ -1,12 +1,21 @@
 WORK_DIR="/mnt/finder/shiyr/code/R1/Search-R1"
 LOCAL_DIR=$WORK_DIR/data/nq_hotpotqa_train_refine_sort3_easy_start
-template_type="refine"
+LOCAL_DIR=$WORK_DIR/data/nq_hotpotqa_train_refine_score
+LOCAL_DIR=$WORK_DIR/data/nq_hotpotqa_train_base_score
+LOCAL_DIR=$WORK_DIR/data/nq_hotpotqa_train_refine_overhaul_2_score
+LOCAL_DIR=$WORK_DIR/data/nq_hotpotqa_train_autorefine
+template_type="autorefine"
+
+# LOCAL_DIR=$WORK_DIR/data/musique_train_base_score
+# template_type="base"
+
 export HF_ENDPOINT=https://hf-mirror.com
 mkdir -p $LOCAL_DIR
 echo "Data Format: $template_type" >> $LOCAL_DIR/datasource.txt
 
 ## process multiple dataset search format train file
 DATA=nq,hotpotqa
+# DATA=musique
 python $WORK_DIR/scripts/data_process/qa_search_train_merge_sort.py --local_dir $LOCAL_DIR --data_sources $DATA --template_type "$template_type"
 echo "Train Data: $DATA" >> $LOCAL_DIR/datasource.txt
 

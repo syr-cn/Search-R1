e66e8cd6cd7168d94d6db1685d2655c25f3c441e
WuChang0124 <c01240214@gmail.com>
2025-05-14 01:34:08 +0000

    update code for barplot (topk)

--- visualize/plot_topk_bar.py
+++ visualize/plot_topk_bar.py
@@ -4,57 +4,65 @@ import numpy as np
 import seaborn as sns
 
 topk = [1, 3, 5, 7]
+color_palette = sns.color_palette("pastel", 4)
+color_palette2 = sns.color_palette("deep", 4)
+
+colors = [
+    '#DCD0FF',
+    '#C6E7FF',
+    '#C6E7FF',
+    'orange',
+]
+
+colors = [
+    color_palette[0],
+    color_palette[1],
+    color_palette[2],
+    color_palette2[-1],
+]
 
 df = pd.read_csv("/mnt/finder/shiyr/code/R1/Search-R1/visualize/data/topk_results.csv")
 
-search_base_df = df[df.iloc[:, 0] == 'Search-R1-base']
-ours_df = df[df.iloc[:, 0] == 'Ours-base']
+searchr1_df = df[df.iloc[:, 0] == 'Search-R1']
+research_df = df[df.iloc[:, 0] == 'ReSearch']
+autorefine_df = df[df.iloc[:, 0] == 'AutoRefine']
 
-ds_names = ['Avg', 'HotpotQA', 'Bamboogle', 'Musique']
-titles = ['(a) Average', '(b) HotpotQA', '(c) Bamboogle', '(d) Musique']
-bar_labels = ['Search-R1-Base', 'AutoRefine-Base']
+bar_labels = ['Search-R1', 'ReSearch', 'AutoRefine']
 line_label = 'Performance Gain'
 
-fig, axes = plt.subplots(1, 4, figsize=(14, 3), sharex=True)
-axes = axes.flatten()
+# Set figure size to 4:3 ratio
+fig, ax = plt.subplots(figsize=(6, 4))
+ax2 = ax.twinx()
 
-bar_width = 0.35
 x = np.arange(len(topk))
-y_lims = [.43,.43,.43,.18,]
-y_lims2 = [.11,.11,.26,.11,]
+bar_width = 0.25
 
+searchr1_acc = searchr1_df['Avg'].values
+research_acc = research_df['Avg'].values
+autorefine_acc = autorefine_df['Avg'].values
 
-for i, ds_name in enumerate(ds_names):
-    ax = axes[i]
-    ax2 = ax.twinx()  
+delta = autorefine_acc - np.max([searchr1_acc, research_acc], axis=0)
 
-    base_acc = search_base_df[ds_name].values
-    ours_acc = ours_df[ds_name].values
-    delta = ours_acc - base_acc
+bars1 = ax.bar(x - bar_width, searchr1_acc, bar_width, label=bar_labels[0], color=colors[0])
+bars2 = ax.bar(x              , research_acc, bar_width, label=bar_labels[1], color=colors[1])
+bars3 = ax.bar(x + bar_width, autorefine_acc, bar_width, label=bar_labels[2], color=colors[2])
 
-    bars1 = ax.bar(x - bar_width/2, base_acc, bar_width, label=bar_labels[0], color='#DCD0FF')
-    bars2 = ax.bar(x + bar_width/2, ours_acc, bar_width, label=bar_labels[1], color='#C6E7FF')
+line, = ax2.plot(x, delta, marker='D', color=colors[3], label=line_label, linestyle='--', linewidth=2)
 
-    line, = ax2.plot(x, delta, marker='o', color='orange', label=line_label,linestyle='--', linewidth=2)
+# ax.set_title('Average', fontsize=12, weight='bold')
+ax.set_xticks(x)
+ax.set_xticklabels([str(k) for k in topk])
+ax.set_xlabel("k", fontsize=12)
+ax.set_ylabel("Accuracy", fontsize=12)
+ax2.set_ylabel("Δ Accuracy", fontsize=12, color=colors[3])
+ax2.tick_params(axis='y', labelcolor=colors[3])
+ax.set_ylim(-0.0, 0.525)
+# ax.set_yticks(np.arange(0.0, 0.5, 0.1))
+ax2.set_ylim(-0.0, 0.105)
 
-    ax.set_title(titles[i], fontsize=12, weight='bold')
-
-    ax.set_xticks(x)
-    ax.set_xticklabels([str(k) for k in topk])
-    ax.set_xlabel("#Doc per Search", fontsize=10)
-
-    if i == 0:
-        ax.set_ylabel("Accuracy", fontsize=10)
-    if i == len(ds_names) - 1:
-        ax2.set_ylabel("Δ Accuracy", fontsize=10, color='orange')
-
-    ax2.tick_params(axis='y', labelcolor='orange')
-    ax.set_ylim(0, y_lims[i])
-    ax2.set_ylim(0, y_lims2[i])
-
-handles = [bars1, bars2, line]
-labels = [bar_labels[0], bar_labels[1], line_label]
-fig.legend(handles, labels, loc='lower center', ncol=3, fontsize=10, frameon=True, bbox_to_anchor=(0.5, -0.01))
-
-plt.tight_layout(rect=[0, 0.1, 1, 1], pad=.2)
-plt.savefig("visualize/figures/barline_top_k_combined.pdf", dpi=300, bbox_inches='tight')
+handles = [bars1, bars2, bars3, line]
+handles = [line, bars1, bars2, bars3]
+labels = [line_label] + bar_labels
+ax.legend(handles, labels, loc='upper left', fontsize=9, frameon=True, ncol=1)
+plt.tight_layout(rect=[0, 0.013, 1, 1])
+plt.savefig("visualize/figures/barline_top_k.pdf", dpi=300, bbox_inches='tight')
